0cd9ba8a10b11646944c4b8f110901ec363d9a39
AAlavilo <121102033+AAlavilo@users.noreply.github.com>
2024-06-01 11:07:40 +0300

    Tuned NavigationBar component
    Now uses anchor tag but will need to change that to <Link> later

--- studyPlatform/src/frontend/components/NavigationBar.jsx
+++ studyPlatform/src/frontend/components/NavigationBar.jsx
@@ -1,102 +1,24 @@
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { useState } from 'react';
+import React from 'react';
+import { Link } from "react-router-dom";
+import "../stylesheets/NavigationBar.css";
 
-const drawerWidth = 240;
-const navItems = ['Home', 'About', 'Contact'];
-
-export default function NavigationBar(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
-  const container = window !== undefined ? () => window().document.body : undefined;
-
+export default function NavigationBar() {
+  //console.log("rendering NavigationBar");
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar component="nav">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            MUI
-          </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            {navItems.map((item) => (
-              <Button key={item} sx={{ color: '#fff' }}>
-                {item}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <nav>
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </nav>
-      <Box component="main" sx={{ p: 3 }}>
-        <Toolbar />
-        
-      </Box>
-    </Box>
+    <nav className="navbar">
+      <div className="navbar-logo">VILO</div>
+      <div className="navbar-tabs">
+        {/*<Link to="/home" className="navbar-tab">Home</Link> This does not work for some reason but would be better in any case. Fix it */}
+        <a href="/home" className="navbar-tab">Home</a>
+        <a href="/profile" className="navbar-tab">Profile</a>
+        <a href="/courses" className="navbar-tab">Courses</a>
+        <a href="/notes" className="navbar-tab">Notes</a>
+        <a href="/my-courses" className="navbar-tab">My Courses</a>
+      </div>
+      <div className="navbar-right">
+        <input type="text" placeholder="Search" className="navbar-search"/>
+        <button className="navbar-login">Login</button>
+      </div>
+    </nav>
   );
-}
+};
