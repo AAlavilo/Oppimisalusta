4b0e649bd2d31e5b11cb6728adc6c9f17cad05d2
Retrobitti <PekkaParviainen@protonmail.com>
2024-06-05 15:29:18 +0300

    Initialized connection to database
    Added functionality to get all users and users by ID. The userScreen now renders name and email from the database using a specific id.(hardcoded id for now).

--- studyPlatform/addUser.js
+++ studyPlatform/addUser.js
@@ -1,15 +1,17 @@
 import connectDB from './connectDB.js';
 import User from './src/models/user.js'; // Adjust the path if necessary
 import pkg from 'mongoose';
+import { getAllUsers } from './src/controllers/userController.js';
 const { connection } = pkg;
+
 
 const addUser = async () => {
   await connectDB();
 
   const newUser = new User({
     firstName: 'Default',
-    lastName: 'Dan',
-    email: 'danny.danny@dandyman.com',
+    lastName: 'Dandelion',
+    email: 'dandelion.danny@dandyman1.com',
     profilePic: 'https://plus.unsplash.com/premium_vector-1711987502039-f12a236104b2?bg=FFFFFF&w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D',
     commenting: true,
     isTeacher: true,
@@ -17,8 +19,12 @@ const addUser = async () => {
   });
 
   try {
-    await newUser.save();
-    console.log('User added:', newUser);
+    //await newUser.save();
+    //console.log('User added:', newUser);
+
+    const users = await getAllUsers();
+    console.log('users:', users)
+    
   } catch (err) {
     console.error('Error adding user:', err.message);
   } finally {
--- studyPlatform/src/frontend/views/UserScreen.jsx
+++ studyPlatform/src/frontend/views/UserScreen.jsx
@@ -2,61 +2,113 @@ import NavigationBar from "../components/NavigationBar"
 import Footer from "../components/footer";
 import { Box, Typography } from "@mui/material";
 import SettingsIcon from '@mui/icons-material/Settings';
+import { useEffect, useState } from "react";
+import profilepic from "/src/assets/default_profile_pic.jpg"
 
 
 
 export default function UserScreen() {
+    const[user, setUser] = useState(null);
+
+    useEffect(() =>{
+        const fetchUserData = async () => {
+            try {
+              const response = await fetch(`http://localhost:3000/api/users/666013ab74dad9fd152f1306`);
+              if (!response.ok) {
+                console.log('Response not ok:', response);
+                throw new Error(`Failed to fetch user data: ${response.status}`);
+              }
+              console.log('Response:', response);
+              const userData = await response.json();
+              console.log('user data: ', userData);
+              setUser(userData);
+            } catch (error) {
+              console.error('Error fetching user data:', error);
+              setUser(null); 
+            }
+          };
+        fetchUserData();
+    }, []);
+    if (user === null) {
+        return (
+            <div>Loading...</div>
+        );
+    }
+
     return (
         <>
-        <NavigationBar />
-            <Box sx={{
-                height: '90vh',
-                width: '100vw',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: "flex-start",
-                alignItems: 'center',
-                backgroundColor: "burlywood",
-                
-            }}>
-                
-                    <Box sx={{ minHeight: '100%', minWidth: '60%', display: 'flex', flexDirection: 'column', alignItems: "center", px: '40px', backgroundColor: "burlywood" }}>
-                        <Typography
-                            variant="h4"
-                            component="div"
-                            sx={{ fontSize: 40, color: "black", backgroundColor: "burlywood"}} // Add padding-left to move it a bit from the edge
-                        >
-                            Olli Opettaja
-                        </Typography>
-                       
-                        <a href="/settings">
-                            <SettingsIcon sx={{ color: "white", fontSize: 100, pt: '20px' }} />
+            <NavigationBar />
+            <Box
+                sx={{
+                    height: '90vh',
+                    width: '100vw',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: "flex-start",
+                    alignItems: 'center',
+                    backgroundColor: "white",
+                }}>
+                <Box
+                    sx={{
+                        height: '100%',
+                        width: '60%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: "flex-start",
+                        px: '40px',
+                        backgroundColor: "white"
+                    }}>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            width: '100%', 
+                            justifyContent: 'space-between' 
+                        }}>
+                        <Box sx={{ display: 'flex', alignItems: 'center', pt: '20px' }}>
+                            <img src={profilepic} alt="Profile" style={{ width: '100px', height: '100px', borderRadius: '50%' }} />
+                            <Typography
+                                variant="h4"
+                                component="div"
+                                sx={{ fontSize: 40, color: "black", pt:'40px', px:'20px'}} 
+                            >
+                                {user.firstName} {user.lastName}
+                            </Typography>
+                        </Box>
+                        <a href="/settings" style={{paddingTop:'90px'}}>
+                            <SettingsIcon sx={{ color: "black", fontSize: 20}} />
                         </a>
+                    </Box>
+                    <Typography
+                        variant="h2"
+                        sx={{
+                            fontSize: 20, color: "white", backgroundColor: "#686868", border: '2px solid black',
+                            width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', pl: '20px', pb: '20px'
+                        }}
+                    >
+                        <h2 style={{fontSize:'20px'}}>Käyttäjätiedot</h2>
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', fontSize:'16px' }}>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <span style={{ marginRight: '10px' }}>{user.email}</span>
+                            </div>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <span style={{ marginRight: '10px' }}>puh. +358358358588</span>
+                            </div>
+                            <div>Lehtori, tieto- ja viestintätekniikka</div>
+                        </div>
+                    </Typography>
+                    <Box sx={{
+                        backgroundColor: "#686868", border: '2px solid black', width: '100%', minHeight: '40%', display: 'flex',
+                        flexDirection: 'column', alignItems: 'flex-start', pl: '20px', pb: '20px', marginTop: '40px'
+                    }}>
                         <Typography
                             variant="h2"
-                            sx={{ fontSize: 20, color: "white", backgroundColor:"#686868", border: '2px solid black', minWidth: '60%', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', pl: '20px', pb: '20px' }} 
-                        >
-                            <h4>Käyttäjätiedot</h4>
-                            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <span style={{ marginRight: '10px' }}>oskari.opettaja@sahkoposti.com</span>
-                                </div>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <span style={{ marginRight: '10px' }}>puh. +358358358588</span>
-                                </div>
-                                <div>Lehtori, tieto- ja viestintätekniikka</div>
-                            </div>
-                        </Typography>
-                        <Box sx={{ backgroundColor:"#686868", border: '2px solid black', minWidth: '60%', minHeight:'40%', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', pl: '20px', pb: '20px', marginTop:'40px'}}>
-                        <Typography
-                        variant="h2"
-                        sx={{ fontSize: 20, color: "white", fontWeight:'bold', pt:'20px'}} 
+                            sx={{ fontSize: 20, color: "white", fontWeight: 'bold', pt: '20px' }}
                         >
                             Kurssit
                         </Typography>
-                        </Box>
                     </Box>
-                
+                </Box>
             </Box>
             <Footer />
         </>
