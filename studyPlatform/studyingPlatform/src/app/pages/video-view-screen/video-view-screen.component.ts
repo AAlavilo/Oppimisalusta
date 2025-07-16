10c9e8a92b6c0e6fe9b6109ff4d3525566e0767b
AAlavilo <alavilo.92@hotmail.com>
2025-07-16 18:02:31 +0300

    Initialize Angular
    Add a new angular project as well as model and route for user

---
+++ studyPlatform/studyingPlatform/src/app/pages/video-view-screen/video-view-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-view-screen',
+  imports: [],
+  templateUrl: './video-view-screen.component.html',
+  styleUrl: './video-view-screen.component.scss'
+})
+export class VideoViewScreenComponent {
+
+}
