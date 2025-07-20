1e98a7eaeec1dc4957cd4d13a9d7c3b4e3a90d3f
Retrobitti <PekkaParviainen@protonmail.com>
2025-07-20 16:59:13 +0300

    example for using data in angular templates
    title

--- studyPlatform/studyingPlatform/src/app/app.routes.ts
+++ studyPlatform/studyingPlatform/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UserScreenComponent } from './pages/user-screen/user-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: UserScreenComponent }, // Default route
+  { path: 'user', component: UserScreenComponent }, // Alternative route
+  { path: '**', redirectTo: '' } // Wildcard route for 404 page
+];
--- studyPlatform/studyingPlatform/src/app/pages/user-screen/user-screen.component.ts
+++ studyPlatform/studyingPlatform/src/app/pages/user-screen/user-screen.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Interface that matches user in database. This is obviously only for demonstration.
+interface User {
+  firstName: string;
+  lastName: string;
+  email: string;
+  profilePic?: string;
+  commenting: boolean;
+  isTeacher: boolean;
+  darkTheme: boolean;
+}
 
 @Component({
   selector: 'app-user-screen',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-screen.component.html',
   styleUrl: './user-screen.component.scss'
 })
-export class UserScreenComponent {
 
+export class UserScreenComponent {
+  // Hardcoded users for demonstratiuon purposes
+  users: User[] = [
+    {
+      firstName: 'Tera',
+      lastName: 'Testi',
+      email: 'tera.testi@testaaja.com',
+      profilePic: 'esimerkki_kuva.png',
+      commenting: true,
+      isTeacher: false,
+      darkTheme: true
+    },
+    {
+      firstName: 'Ella',
+      lastName: 'Esimerkki',
+      email: 'ella.esi@merkiksi.com',
+      profilePic: 'esimerkki_kuva.png',
+      commenting: false,
+      isTeacher: true,
+      darkTheme: false
+    },
+  ];
 }
